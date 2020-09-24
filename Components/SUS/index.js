dc8f1cb5732061e18410cbcd2115cb296b87bd0f
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2020-09-23 23:42:52 -0400

    created a components folder to store the bot functions

---
+++ Components/SUS/index.js
@@ -0,0 +1,40 @@
+const SUS = (message) => {
+  if (message.content === "SUS") {
+    let randomVar = Math.floor(Math.random() * 12 + 1);
+    switch (randomVar) {
+      case 0:
+        message.reply("Yellow did it!. Kick him of the ship");
+        break;
+      case 1:
+        message.reply("Blue did it!. Kick him of the ship");
+        break;
+      case 2:
+        message.reply("Orange did it!. Kick him of the ship");
+        break;
+      case 3:
+        message.reply("Purple did it!. Kick him of the ship");
+        break;
+      case 4:
+        message.reply("Cyan did it!. Kick him of the ship");
+        break;
+      case 5:
+        message.reply("Brown did it!. Kick him of the ship");
+        break;
+      case 6:
+        message.reply("Red did it!. Kick him of the ship");
+        break;
+      case 7:
+        message.reply("Black did it!. Kick him of the ship");
+        break;
+      case 8:
+        message.reply("White did it!. Kick him of the ship");
+        break;
+      case 9:
+        message.reply("Lime did it!. Kick him of the ship");
+        break;
+      default:
+        message.reply("Pink did it!. Kick him of the ship");
+    }
+  }
+};
+export default SUS;
--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
+import SUS from './Components/SUS/index'
 
  
 
@@ -17,53 +17,11 @@ client.on('message', message => {
        message.reply('Hello, welcome to our discord channel. I am Eugene');
 
        }
+    SUS(message);
+
 
 });
 
-
-client.on('message', message => {
-
-    if (message.content === 'SUS') {
-        let randomVar = Math.floor((Math.random() *(12) + 1));
-        switch(randomVar) {
-            case 0:
-              message.reply('Yellow did it!. Kick him of the ship')
-              break;
-            case 1:
-                message.reply('Blue did it!. Kick him of the ship')
-              break;
-              case 2:
-                message.reply('Orange did it!. Kick him of the ship')
-              break;
-              case 3:
-                message.reply('Purple did it!. Kick him of the ship')
-              break;
-              case 4:
-                message.reply('Cyan did it!. Kick him of the ship')
-              break;
-              case 5:
-                message.reply('Brown did it!. Kick him of the ship')
-              break;
-              case 6:
-                message.reply('Red did it!. Kick him of the ship')
-              break;
-              case 7:
-                message.reply('Black did it!. Kick him of the ship')
-              break;
-              case 8:
-                message.reply('White did it!. Kick him of the ship')
-              break;
-              case 9:
-                message.reply('Lime did it!. Kick him of the ship')
-                break;
-            default:
-                message.reply('Pink did it!. Kick him of the ship')
-          }
-}
-
-});
-
- 
 
 // THIS  MUST  BE  THIS  WAY
 
