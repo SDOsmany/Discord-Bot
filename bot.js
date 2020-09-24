4ba456dfc47df1683da5b9849513306dfad07ad2
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2020-09-23 23:05:07 -0400

    added the SUS function

--- bot.js
+++ bot.js
@@ -10,8 +10,6 @@ client.on('ready', () => {
 
 });
 
- 
-
 client.on('message', message => {
 
     if (message.content === 'Hello') {
@@ -19,6 +17,46 @@ client.on('message', message => {
        message.reply('Hello, welcome to our discord channel. I am Eugene');
 
        }
+
+});
+
+
+client.on('message', message => {
+
+    if (message.content === 'SUS') {
+        let randomVar = Math.floor((Math.random() * 10));
+        switch(randomVar) {
+            case 0:
+              message.reply('Yellow did it!. Kick him of the ship')
+              break;
+            case 1:
+                message.reply('Blue did it!. Kick him of the ship')
+              break;
+              case 2:
+                message.reply('Orange did it!. Kick him of the ship')
+              break;
+              case 3:
+                message.reply('Purple did it!. Kick him of the ship')
+              break;
+              case 4:
+                message.reply('Cyan did it!. Kick him of the ship')
+              break;
+              case 5:
+                message.reply('Brown did it!. Kick him of the ship')
+              break;
+              case 6:
+                message.reply('Red did it!. Kick him of the ship')
+              break;
+              case 7:
+                message.reply('Black did it!. Kick him of the ship')
+              break;
+              case 8:
+                message.reply('White did it!. Kick him of the ship')
+              break;
+            default:
+                message.reply('Pink did it!. Kick him of the ship')
+          }
+}
 
 });
 
