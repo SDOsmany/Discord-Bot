618ea4b4940bc39bde2e7284f48d53594610fed9
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-02-09 11:35:47 -0500

    new

--- bot.js
+++ bot.js
@@ -1,24 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const sus = require("./Components/SUS/index");
-
  
 
 client.on('ready', () => {
 
     console.log('I am ready!');
-
-});
-
-client.on('message', message => {
-
-    if (message.content === 'Hello') {
-
-       message.reply('Hello, welcome to our discord channel. I am Eugene');
-
-       }
-    sus(message);
-
 
 });
 
