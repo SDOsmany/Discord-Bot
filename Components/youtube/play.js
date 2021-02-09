3c1afa98859f429d780417ca9957ad6189bd5166
Osmany <58539319+SDOsmany@users.noreply.github.com>
2021-02-09 01:03:07 -0500

    Delete Components directory

--- Components/youtube/play.js
+++
@@ -1,9 +0,0 @@
-module.exports.run=async(client,message,args)=>{
-    const music = args.join(" ");
-    client.distubnoe.play(message,music)
-}
-
-module.exports.config = {
-    name: "play",
-    aliases:['p']
-}
