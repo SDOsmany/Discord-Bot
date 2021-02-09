1cb2ce5fb3ad2b305166a158ad3e21c599d017f9
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-02-09 12:15:08 -0500

    added the .env and imported it to the bot.js file

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+require('dotenv').config()
  
 
 client.on('ready', () => {
@@ -7,8 +8,8 @@ client.on('ready', () => {
     console.log('I am ready!');
 
 });
-
+console.log(process.env.TOKEN)
 
 // THIS  MUST  BE  THIS  WAY
 
-// client.login(process.env.TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.TOKEN);//BOT_TOKEN is the Client Secret
