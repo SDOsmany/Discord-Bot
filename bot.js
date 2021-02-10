4f6fab330e9adb01259b4cc5be6a8af5581f759a
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-02-09 19:33:25 -0500

    documatation change

--- bot.js
+++ bot.js
@@ -1,34 +1,108 @@
-const Discord = require('discord.js');
-require('dotenv').config()
+// const Discord = require('discord.js');
+// require('dotenv').config()
+
+// //MUSIC --------------------------------------------------------------------------------------------------
+
+// const bot = new Discord.Client({
+// 	partials: ['MESSAGE', 'CHANNEL', 'REACTION']
+// });
+// const config = require('./settings.json');
+// const { loadCommands } = require('./utils/loadCommands');
+// const DisTube = require('distube')
+
+// bot.distube = new DisTube(bot, { searchSongs: false, emitNewSongOnly: true });
+// bot.distube
+//     .on("playSong", (message, queue, song) => message.channel.send(
+//         `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}`
+// 	))
+// 	.on("addSong", (message, queue, song) => message.channel.send(
+//         `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+//     ))
+
+// require('./utils/loadEvents')(bot);
+
+// bot.commands = new Discord.Collection();
+// bot.aliases = new Discord.Collection();
+
+// loadCommands(bot);
 
 //MUSIC --------------------------------------------------------------------------------------------------
+const Discord = require('discord.js'),
+    DisTube = require('distube'),
+    client = new Discord.Client(),
+    config = {
+        prefix: "!",
+    };
 
-const bot = new Discord.Client({
-	partials: ['MESSAGE', 'CHANNEL', 'REACTION']
+// Create a new DisTube
+const distube = new DisTube(client, { searchSongs: true, emitNewSongOnly: true });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 });
-const config = require('./settings.json');
-const { loadCommands } = require('./utils/loadCommands');
-const DisTube = require('distube')
 
-bot.distube = new DisTube(bot, { searchSongs: false, emitNewSongOnly: true });
-bot.distube
+client.on("message", async (message) => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(config.prefix)) return;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift();
+
+    if (command == "play")
+        distube.play(message, args.join(" "));
+
+    if (["repeat", "loop"].includes(command))
+        distube.setRepeatMode(message, parseInt(args[0]));
+
+    if (command == "stop") {
+        distube.stop(message);
+        message.channel.send("Stopped the music!");
+    }
+
+    if (command == "skip")
+        distube.skip(message);
+
+    if (command == "queue") {
+        let queue = distube.getQueue(message);
+        message.channel.send('Current queue:\n' + queue.songs.map((song, id) =>
+            `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``
+        ).slice(0, 10).join("\n"));
+    }
+
+    if ([`3d`, `bassboost`, `echo`, `karaoke`, `nightcore`, `vaporwave`].includes(command)) {
+        let filter = distube.setFilter(message, command);
+        message.channel.send("Current queue filter: " + (filter || "Off"));
+    }
+});
+
+// Queue status template
+const status = (queue) => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode == 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
+
+// DisTube event listeners, more in the documentation page
+distube
     .on("playSong", (message, queue, song) => message.channel.send(
-        `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}`
-	))
-	.on("addSong", (message, queue, song) => message.channel.send(
+        `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${status(queue)}`
+    ))
+    .on("addSong", (message, queue, song) => message.channel.send(
         `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
     ))
+    .on("playList", (message, queue, playlist, song) => message.channel.send(
+        `Play \`${playlist.name}\` playlist (${playlist.songs.length} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
+    ))
+    .on("addList", (message, queue, playlist) => message.channel.send(
+        `Added \`${playlist.name}\` playlist (${playlist.songs.length} songs) to queue\n${status(queue)}`
+    ))
+    // DisTubeOptions.searchSongs = true
+    .on("searchResult", (message, result) => {
+        let i = 0;
+        message.channel.send(`**Choose an option from below**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`);
+    })
+    // DisTubeOptions.searchSongs = true
+    .on("searchCancel", (message) => message.channel.send(`Searching canceled`))
+    .on("error", (message, e) => {
+        console.error(e)
+        message.channel.send("An error encountered: " + e);
+    });
 
-require('./utils/loadEvents')(bot);
-
-bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
-
-loadCommands(bot);
-
-//MUSIC --------------------------------------------------------------------------------------------------
 
 
-
-
-bot.login(process.env.TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.TOKEN);//BOT_TOKEN is the Client Secret
