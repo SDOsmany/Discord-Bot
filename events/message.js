d333261a371af28b2c5dcb77b77d3b963b9e7be1
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-02-09 19:07:14 -0500

    changes

--- events/message.js
+++ events/message.js
@@ -1,25 +1,25 @@
-module.exports = async (client, message) => {
-  if (message.author.client) return;
+module.exports = async (bot, message) => {
+  if (message.author.bot) return;
 
   const messageArray = message.content.split(" ");
   const cmd = messageArray[0];
   const args = messageArray.slice(1);
 
-  if (message.author.client || message.channel.type === "dm") return;
+  if (message.author.bot || message.channel.type === "dm") return;
   const prefix = "?";
 
-  if (message.content.match(new RegExp(`^<@!?${client.user.id}>( |)$`)))
+  if (message.content.match(new RegExp(`^<@!?${bot.user.id}>( |)$`)))
     return message.channel.send(
       `${message.guild.name}'s Prefix is \`${prefix}\`\n\nTo get a list of commands, say \`${prefix}help\``
     );
 
   if (!message.content.startsWith(prefix)) return;
   const commandfile =
-    client.commands.get(cmd.slice(prefix.length).toString().toLowerCase()) ||
-    client.commands.get(
-      client.aliases.get(cmd.slice(prefix.length).toString().toLowerCase())
+    bot.commands.get(cmd.slice(prefix.length).toString().toLowerCase()) ||
+    bot.commands.get(
+      bot.aliases.get(cmd.slice(prefix.length).toString().toLowerCase())
     );
   if (commandfile) {
-    commandfile.run(client, message, args);
+    commandfile.run(bot, message, args);
   }
 };
